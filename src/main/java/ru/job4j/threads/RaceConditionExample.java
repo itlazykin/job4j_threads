5e1d208f07e9f26621848d88ec004438f9b3184c
itlazykin <dzaka89@gmail.com>
2024-12-08 12:19:45 +0300

    5. Состояние гонки [#504909 #524844]. Пример с лекции.

---
+++ src/main/java/ru/job4j/threads/RaceConditionExample.java
@@ -0,0 +1,26 @@
+package ru.job4j.threads;
+
+public class RaceConditionExample {
+    private final int number = 0;
+
+    public synchronized void increment() {
+        for (int i = 0; i < 99999; i++) {
+            int current = number;
+            int next = number + 1;
+            System.out.println(i);
+            if (current + 1 != next) {
+                throw new IllegalStateException("Некорректное сравнение: " + current + " + 1 = " + next);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        RaceConditionExample raceCondition = new RaceConditionExample();
+        Thread one = new Thread(raceCondition::increment);
+        one.start();
+        Thread two = new Thread(raceCondition::increment);
+        two.start();
+        one.join();
+        two.join();
+    }
+}
